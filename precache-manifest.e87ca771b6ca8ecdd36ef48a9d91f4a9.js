bfc331adaf6b58e848a79308c16a375dffbc2af8
Santiano <Arjimson.Santiano@nchcss.com>
2019-04-09 15:14:59 +0800

    Updates

---
+++ precache-manifest.e87ca771b6ca8ecdd36ef48a9d91f4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-upload/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3aa59d1f23e7ceed144",
+    "url": "/my-first-upload/static/js/runtime~main.92c39714.js"
+  },
+  {
+    "revision": "d42c31e3b429a54d6a26",
+    "url": "/my-first-upload/static/js/main.975d4ec9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-first-upload/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d42c31e3b429a54d6a26",
+    "url": "/my-first-upload/static/css/main.2925e13f.chunk.css"
+  },
+  {
+    "revision": "42157e106898ce808ac47e61ad338f5c",
+    "url": "/my-first-upload/index.html"
+  }
+];
